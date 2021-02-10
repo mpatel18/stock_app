f2fb21dcf865d995d7a601c47435c270f465e493
mpatel18 <meekitpatel@gmail.com>
2021-02-10 15:02:29 -0500

    added bulma

--- pages/index.js
+++ pages/index.js
@@ -8,15 +8,25 @@ import styles from '../styles/Home.module.sass'
         <title>Stock Lookup</title>
       </Head>
       <main>
-        <h1 className={styles.title}>
-          Stock Lookup
-        </h1>
+        <h1 className={styles.title}>Stock Lookup</h1>
         <div className={styles.search}>
-          <input placeholder="Search" type="text"></input>
+          <input placeholder="Search" type="text" required></input>
         </div>
       </main>
     </div>
   )
 }
+
+// Fetch Ticker Data
+// export const getStaticProps = async () => {
+//   const res = await fetch(`https://api.polygon.io/v2/reference/tickers?market=STOCKS&locale=us&search=gamestop&perpage=1&page=1&apiKey=nSmGnGWdkG6rUSFIlTXK7agr0DXQexoa`)
+//   const tickers = await res.json();
+
+//   return { 
+//       props: {
+//           tickers
+//       }
+//   }
+// }
 
 export default Home
